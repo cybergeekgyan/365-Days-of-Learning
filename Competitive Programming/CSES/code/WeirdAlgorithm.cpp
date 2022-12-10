36e12b8532943ac65947fd41c68ebdf59a209e67
Gyan Prakash <studyoneminute@gmail.com>
2022-12-10 10:10:39 -0800

    Add files via upload

---
+++ Competitive Programming/CSES/code/WeirdAlgorithm.cpp
@@ -0,0 +1,27 @@
+// //Weird Algorithm
+
+#include <iostream> 
+
+using namespace std; 
+
+int main() 
+{ 
+    
+    long n; 
+    cin >> n; 
+  
+    while (n != 1) 
+    { 
+        cout << n << " "; 
+  
+        if (n % 2 == 0) 
+            n = n / 2; 
+        else
+            n = (n * 3) + 1; 
+    } 
+  
+    cout << "1"<<endl; 
+  
+    return 0; 
+}
+
