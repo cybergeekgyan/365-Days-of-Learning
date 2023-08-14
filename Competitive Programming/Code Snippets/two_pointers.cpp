25e8394daaf84d0b4b9d2ca6438cc64b7aaddb08
Gyan Prakash <studyoneminute@gmail.com>
2023-08-15 00:00:49 +0530

    Create two_pointers.cpp

---
+++ Competitive Programming/Code Snippets/two_pointers.cpp
@@ -0,0 +1,13 @@
+int f(vector<int>& v) {
+    int ans = 0;
+    int l = 0, r = (int) v.size() - 1;
+    while (l < r) {
+        // do some logic here
+        if (/* condition */) {
+            l += 1;
+        } else {
+            r -= 1;
+        }
+    }
+    return ans;
+}
