9b06463672d36011136c1b6d33c9b8868dd44f40
Gyan Prakash <studyoneminute@gmail.com>
2023-08-15 00:05:07 +0530

    Create topological_sort.cpp

---
+++ Competitive Programming/Code Snippets/graph_theory/topological_sort.cpp
@@ -0,0 +1,32 @@
+struct TopologicalSort {
+    int n;
+    vector<int> indegree;
+    vector<int> orders;
+    vector<vector<int>> G;
+    bool isTopologicalSorted = false;
+    
+    TopologicalSort(vector<vector<int>>& g, vector<int>& in) {
+        G = g; vector<vector<int>>
+        n = (int) G.size();
+        indegree = in;
+        
+        int res = 0;
+        queue<int> q;
+        for(int i = 0; i < n; i++) {
+            if(indegree[i] == 0) {
+                q.push(i);
+            }
+        }
+        while(!q.empty()) {
+            auto u = q.front(); q.pop();
+            orders.push_back(u);
+            for(auto v : G[u]) {
+                if(--indegree[v] == 0) {
+                    q.push(v);
+                }
+            }
+            res++;
+        }
+        isTopologicalSorted = res == n;
+    }
+};
